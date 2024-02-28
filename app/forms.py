01dfd97c7f144607057643084e373b3653117356
Brittany Thomas <yaneelthomas162@gmail.com>
2024-02-28 18:58:30 -0500

    Completed Code

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired, Email
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    email = StringField('E-mail', validators=[InputRequired(), Email()])
+    subject = StringField('Subject', validators=[InputRequired()])
+    message = TextAreaField('Message', validators=[InputRequired()])
+
+    
+
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -16,6 +18,27 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    form = ContactForm()
+    
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            subject = form.email.data
+            message = form.message.data
+            
+            msg = Message(subject, sender=(name, email), recipients=["yaneelthomas162@gmail.com"]) 
+            msg.body = message
+            mail.send(msg)
+
+            flash('Your email was sent sucessfully!', 'success')
+            return redirect('/')
+        
+        flash_errors(form)
+    return render_template('contact.html', form=form)            
 
 
 ###
